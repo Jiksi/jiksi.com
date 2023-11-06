b5ab4127af20302cb9ffefc820f188aa84979b8f
Zhicxi Azis Pramana <zhicxiap99@gmail.com>
2023-11-06 20:14:02 +0800

    feat: add project component

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Project() {
-  return <div className="h-screen">Project</div>;
+  return (
+    <section className="h-screen container">
+      <h3 className="font-medium text-2xl underline-offset-2 underline mb-5">
+        Project
+      </h3>
+      <p className="text-xl">Coming soon.</p>
+    </section>
+  );
 }
