0d4a75cee0fe306a286d1ee4fdf9fa4fae1934bf
Zhicxi Azis Pramana <zhicxiap99@gmail.com>
2023-11-06 20:13:52 +0800

    feat: add home component

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,5 +1,39 @@
-import React from "react";
+"use client";
 
+import React from "react";
+import Image from "next/image";
+import jiksi from "/public/img/jiksi.png";
+import { motion } from "framer-motion";
 export default function Home() {
-  return <div className="h-screen">Home</div>;
+  return (
+    <section className="min-h-main flex flex-col justify-center container">
+      <div className="flex justify-around">
+        <motion.div
+          className="flex flex-col justify-center"
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.6 }}
+        >
+          <h2 className="font-extrabold text-4xl">Hello World!</h2>
+          <h2 className="font-extrabold text-5xl">
+            You can call me&nbsp;
+            <span className="text-primary-foreground bg-primary rounded-md px-5">
+              Jiksi
+            </span>
+          </h2>
+          <div className="font-medium text-2xl mt-5 text-muted-foreground">
+            <p>Building awesome website with React,</p>
+            <p>Typescript, and Framer Motion</p>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.6 }}
+        >
+          <Image src={jiksi} alt="jiksi" height={400} width={400} />
+        </motion.div>
+      </div>
+    </section>
+  );
 }
