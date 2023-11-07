c7fc46ea9fa41399646db164379505759dd460d9
Zhicxi Azis Pramana <zhicxiap99@gmail.com>
2023-11-07 14:15:11 +0800

    feat: update and styling header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,32 @@
 import React from "react";
+import Link from "next/link";
+
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { ModeToggle } from "@/components/theme-switcher";
-import { Separator } from "./ui/separator";
-import { Avatar, AvatarImage } from "./ui/avatar";
+import { Separator } from "@/components/ui/separator";
 
 export default function Header() {
   return (
     <header className="sticky top-0 bg-background text-foreground">
-      <nav className="flex items-center container justify-between h-20 text-base md:text-xl">
-        <div className="flex items-center">
-          <Avatar className="h-9 w-9 mr-4">
-            <AvatarImage src="images/logo.png" alt="Jiksi Logo" />
-          </Avatar>
-          <h1 className="font-bold">JIKSI</h1>
-        </div>
-        <ul className="flex items-center gap-4 md:gap-10 font-medium">
-          <li>Project</li>
-          <li>About</li>
-          <li>Contact</li>
+      <nav className="container flex h-20 items-center justify-between text-base md:text-xl">
+        <Link href="/">
+          <div className="flex items-center">
+            <Avatar className="mr-4 h-9 w-9">
+              <AvatarImage src="svg/logo.svg" alt="Jiksi Logo" />
+            </Avatar>
+            <h1 className="font-bold">JIKSI</h1>
+          </div>
+        </Link>
+        <ul className="flex items-center gap-4 font-medium md:gap-10">
+          <li>
+            <Link href="/#project">Project</Link>
+          </li>
+          <li>
+            <Link href="/#about">About</Link>
+          </li>
+          <li>
+            <Link href="/#contact">Contact</Link>
+          </li>
           <ModeToggle />
         </ul>
       </nav>
