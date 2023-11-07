e657dbf346092931f91cb8336f59f870d5ac508d
Zhicxi Azis Pramana <zhicxiap99@gmail.com>
2023-11-07 14:14:10 +0800

    feat: add about and contact to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Home from "@/components/home";
 import Project from "@/components/project";
+import About from "@/components/about";
+import Contact from "@/components/contact";
 
 export default function RootPage() {
   return (
     <main>
       <Home />
       <Project />
+      <About />
+      <Contact />
     </main>
   );
 }
