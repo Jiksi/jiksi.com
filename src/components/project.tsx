c271f7c574b2eb54f40cfb1fe33780696e848f5b
Zhicxi Azis Pramana <zhicxiap99@gmail.com>
2023-11-07 14:15:35 +0800

    feat: update project section

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Project() {
   return (
-    <section className="h-screen container">
-      <h3 className="font-medium text-2xl underline-offset-2 underline mb-5">
+    <section className="container mb-20 md:mb-40" id="project">
+      <h3 className="mb-5 text-2xl font-bold underline underline-offset-4">
         Project
       </h3>
       <p className="text-xl">Coming soon.</p>
