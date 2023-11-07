8f72fc6f65812fe36eb642e55c161ae5d8c51e3e
Zhicxi Azis Pramana <zhicxiap99@gmail.com>
2023-11-07 14:14:37 +0800

    feat: add content to contact section

---
+++ src/components/contact.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <section className="container mb-10 md:mb-20" id="contact">
+      <h3 className="mb-5 text-2xl font-bold underline underline-offset-4">
+        Contact
+      </h3>
+      <p className="w-full text-lg leading-loose md:w-1/2 md:text-xl">
+        If you&apos;re interested in collaborating, have any questions, or just
+        want to discuss exciting projects, feel free to get in touch with me.
+        I&apos;m always happy to communicate and connect with new friends.
+        <p>(find my contact in the footer)</p>
+      </p>
+    </section>
+  );
+}
